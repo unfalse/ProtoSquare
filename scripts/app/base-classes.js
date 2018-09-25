b39563eb2ce94f275bbd60bfddc88176b5eaafc6
Iskander Gubaidullin <Iskander_Gubaidullin@epam.com>
2018-09-25 10:57:42 +0300

    WIP: started with constants and base

---
+++ scripts/app/base-classes.js
@@ -0,0 +1,20 @@
+define(function() {
+    var MapObject = function(id, x, y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.hide = false;
+      };
+    MapObject.prototype.enabled = true;
+
+    var NothingObject = function(x, y) {
+        MapObject.apply(this, [NOTHING.ID, x, y]);
+      }
+    NothingObject.prototype = Object.create(MapObject.prototype);
+    NothingObject.prototype.constructor = NothingObject;
+
+    return {
+        MapObject: MapObject
+        ,NothingObject: NothingObject
+    }
+});
